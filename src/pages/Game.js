ce269ab69bf4b312c676023342cbb72c5464aeb8
Thiago Ximenes <thiagoximenes@pop-os.localdomain>
2022-01-13 18:19:32 -0300

    insert header into game page.

---
+++ src/pages/Game.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from '../components/game/Header';
+
+export default class componentName extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    )
+  }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from '../pages/Login';
+import Game from '../pages/Game';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/game" component={ Game } />
       </Switch>
     );
   }
