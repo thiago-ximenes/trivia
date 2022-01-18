05ce389df7a00a7cd6ceb228165bef1ee314ebd0
Felipe David <felipe.besorean@hotmail.com>
2022-01-18 17:31:23 -0300

    create feedback structure, still needs localStore to work

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
+const result = 5;
+const average = 3;
+
 class Feedback extends Component {
+  // precisamos pegar do localStorage o numero de acertos
+  feedbackMessage = () => {
+    if (result < average) {
+      return (
+        <div>
+          Could be better...
+        </div>);
+    } return (
+      <div>
+        Well Done!
+      </div>);
+  };
+
   render() {
     return (
       <div>
-        <h1 data-testid="feedback-text">
-          Bem Vindo
-        </h1>
+        <div data-testid="feedback-text">
+          { this.feedbackMessage() }
+        </div>
       </div>
     );
   }
